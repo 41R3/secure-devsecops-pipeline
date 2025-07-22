02a10cb50cb380898e5d39bc16dca2a5aece2429
41R3 <selin199116@gmail.com>
2025-07-22 18:54:44 -0500

    Fix pipeline y añade headers de seguridad

--- app/app.py
+++ app/app.py
@@ -1,5 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+# Middleware para headers de seguridad
+@app.after_request
+def add_security_headers(response):
+    # Headers para resolver las vulnerabilidades detectadas
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['Content-Security-Policy'] = "default-src 'self'"
+    response.headers['Permissions-Policy'] = "geolocation=(self)"
+    response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin'
+    response.headers['Server'] = 'SecureServer'  # Ocultar versión del servidor
+    return response
 
 @app.route('/')
 def home():
